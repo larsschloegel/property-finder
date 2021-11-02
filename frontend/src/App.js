30e33596f01948876b7ba1df01719e7feacab170
Lars Schlögel <lars.schloegel@gmail.com>
2021-11-02 10:22:11 +0100

    Move BrowserRoute

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import styled, {ThemeProvider} from "styled-components";
 import stylesheet from "./styles/Stylesheet";
-import {Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import SearchInputs from "./pages/SearchInputs";
 import Properties from "./pages/Properties";
@@ -12,21 +12,23 @@ export default function App() {
   return (
       <ThemeProvider theme={stylesheet}>
         <PageLayout>
-          <Header/>
-          <Switch>
-            <Route exact path="/">
-              <Homepage/>
-            </Route>
-            <Route exact path="/search-inputs">
-              <SearchInputs/>
-            </Route>
-            <Route exact path="/properties">
-              <Properties/>
-            </Route>
-            <Route exact path="/login">
-              <Login/>
-            </Route>
-          </Switch>
+          <Router>
+            <Header/>
+            <Switch>
+              <Route exact path="/">
+                <Homepage/>
+              </Route>
+              <Route exact path="/search-inputs">
+                <SearchInputs/>
+              </Route>
+              <Route exact path="/properties">
+                <Properties/>
+              </Route>
+              <Route exact path="/login">
+                <Login/>
+              </Route>
+            </Switch>
+          </Router>
         </PageLayout>
       </ThemeProvider>
 
